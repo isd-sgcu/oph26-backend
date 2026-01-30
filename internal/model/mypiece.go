64ee48967f31d95392e21029d63259c295aec9e6
Napongthorn <6631313321@student.chula.ac.th>
2026-01-30 14:56:41 +0700

    feat: add mypiece model

---
+++ internal/model/mypiece.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type MyPiece struct {
+	ID         uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	UserID     uuid.UUID `gorm:"type:uuid;not null"`
+	User       User      `gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	PieceCode  string    `gorm:"type:text;uniqueIndex;not null"`
+	ExpireDate time.Time `gorm:"not null"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
