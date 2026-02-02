fd7b7e6c4c6450d09a203c1bfe1d1106cba9c6bf
Napongthorn <6631313321@student.chula.ac.th>
2026-02-02 15:35:39 +0700

    Fix: link to Attendee

--- internal/model/mypiece.go
+++ internal/model/mypiece.go
@@ -8,8 +8,8 @@ import (
 
 type MyPiece struct {
 	ID         uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	UserID     uuid.UUID `gorm:"type:uuid;not null"`
-	Attendee   Attendee  `gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	AttendeeID uuid.UUID `gorm:"column:user_id;type:uuid;not null"`
+	Attendee   Attendee  `gorm:"foreignKey:AttendeeID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	PieceCode  string    `gorm:"type:text;uniqueIndex;not null"`
 	ExpireDate time.Time `gorm:"not null"`
 	CreatedAt  time.Time
