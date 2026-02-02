a191d38d0e922a8c2253aace9da2fd5b85e88aa1
Napongthorn <6631313321@student.chula.ac.th>
2026-02-02 07:19:56 +0700

    Fix: link models to Attendee

--- internal/model/collectedpiece.go
+++ internal/model/collectedpiece.go
@@ -9,7 +9,7 @@ import (
 type CollectedPiece struct {
 	UserID      uuid.UUID `gorm:"type:uuid;primaryKey;not null"`
 	PieceID     uuid.UUID `gorm:"type:uuid;primaryKey;not null"`
-	User        User      `gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Attendee    Attendee  `gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	MyPiece     MyPiece   `gorm:"foreignKey:PieceID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	CollectedAt time.Time `gorm:"not null"`
 	CreatedAt   time.Time
--- internal/model/mypiece.go
+++ internal/model/mypiece.go
@@ -9,7 +9,7 @@ import (
 type MyPiece struct {
 	ID         uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	UserID     uuid.UUID `gorm:"type:uuid;not null"`
-	User       User      `gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Attendee   Attendee  `gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	PieceCode  string    `gorm:"type:text;uniqueIndex;not null"`
 	ExpireDate time.Time `gorm:"not null"`
 	CreatedAt  time.Time
