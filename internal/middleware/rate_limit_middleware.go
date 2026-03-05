a3233eae95699a05bc04cb05215c1ac04a73a493
Patsagorn Yuenyong <works@ptsgrn.dev>
2026-03-05 23:27:31 +0700

    feat: add rate limit middleware using sliding window limiter

---
+++ internal/middleware/rate_limit_middleware.go
@@ -0,0 +1,17 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+)
+
+func NewRateLimitMiddleware() fiber.Handler {
+	// Using a sliding window limiter to allow bursts of requests while still enforcing an overall rate limit.
+	return limiter.New(limiter.Config{
+		Max:               20,
+		Expiration:        30 * time.Second,
+		LimiterMiddleware: limiter.SlidingWindow{},
+	})
+}
