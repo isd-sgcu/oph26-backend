b67fe1e9f0e69f79ba3e8eb447871aa6aaced112
Patsagorn Yuenyong <works@ptsgrn.dev>
2026-03-12 20:26:21 +0700

    feat(checkin): update Checkin schema to rename UserID to AttendeeID and adjust CheckedInAt field

--- internal/entity/checkin.go
+++ internal/entity/checkin.go
@@ -8,9 +8,9 @@ import (
 
 type Checkin struct {
 	ID          uuid.UUID `gorm:"type:uuid;primaryKey;not null"`
-	UserID      uuid.UUID `gorm:"type:uuid;not null;index:idx_user_faculty"`
+	AttendeeID  uuid.UUID `gorm:"type:uuid;not null;index:idx_user_faculty"`
 	Faculty     string    `gorm:"not null;index:idx_user_faculty"`
 	StaffID     uuid.UUID `gorm:"type:uuid;not null"`
 	Staff       Staff     `gorm:"foreignKey:StaffID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	CheckedInAt time.Time `gorm:"not null"`
+	CheckedInAt time.Time `gorm:"autoCreateTime"`
 }
